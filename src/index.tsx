6ecbb441ef40b14afcda5e737fc75aa6809c974d
usmor <7887330@gmail.com>
2025-09-05 14:54:42 +0300

    feat(webpack): добавляет сборку и линтеры

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import "./styles/variables.css";
+import "./styles/globals.css";
---
+++ webpack/webpack.common.js
@@ -0,0 +1,106 @@
+const HTMLWebpackPlugins = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const production = process.env.NODE_ENV === "production";
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "..", "./dist"),
+    filename: production
+      ? "static/scripts/[name].[contenthash].js"
+      : "static/scripts/[name].js",
+    publicPath: "/",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(ico|png|svg|jpg|jpeg|gif|webp)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "static/images/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "static/fonts/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ["@svgr/webpack", "url-loader"],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          production ? MiniCssExtractPlugin.loader : "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                mode: "local",
+                localIdentName: "[name]__[local]__[hash:base64:5]",
+                namedExport: false,
+                auto: /\.module\.\w+$/i,
+              },
+              importLoaders: 2,
+            },
+          },
+          "postcss-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".tsx", ".ts", ".json"],
+  },
+  plugins: [
+    new HTMLWebpackPlugins({
+      template: path.resolve(__dirname, "..", "./public/index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "static/styles/[name].[contenthash].css",
+    }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: "development",
+    }),
+    new FaviconsWebpackPlugin({
+      logo: path.resolve(__dirname, "..", "./public/favicon.svg"),
+      mode: "webapp",
+      devMode: "webapp",
+      favicons: {
+        appName: "-",
+        appDescription: "-",
+        developerName: "-",
+        developerURL: null,
+        background: "#ddd",
+        theme_color: "#333",
+        icons: {
+          coast: false,
+          yandex: false,
+        },
+      },
+    }),
+  ],
+};
