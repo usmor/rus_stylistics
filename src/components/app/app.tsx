1d347662f1f7f2ef82c4b29df8e3275bbf4bfea2
usmor <7887330@gmail.com>
2025-09-15 16:18:25 +0300

    fix(menu): при перезагрузке страницы меню не закрывается

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,20 +1,36 @@
 import styles from "./app.module.css";
 import { Header } from "../header/header";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import { Footer } from "../footer/footer";
 import { Menu } from "../menu/menu";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash === "#menu") {
+      setIsMenuOpen(true);
+    } else {
+      setIsMenuOpen(false);
+    }
+  }, [location.hash]);
 
   const handleMenuOpen = () => {
-    setIsMenuOpen(true);
+    navigate(`${location.pathname}${location.search}#menu`, { replace: true });
   };
 
   const handleMenuClose = () => {
-    setIsMenuOpen(false);
+    if (location.hash === "#menu") {
+      navigate(`${location.pathname}${location.search}`, { replace: true });
+    }
   };
+
+  useEffect(() => {
+    setIsMenuOpen(location.hash === "#menu");
+  }, [location.hash]);
 
   return (
     <div className={styles.app}>
